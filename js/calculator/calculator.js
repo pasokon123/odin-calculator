2107ab66af0eb69eb21a418ac9bb1ac19e952af9
ae225aw <ae225aw@student.lnu.se>
2025-02-27 15:32:48 +0900

    Make the shape of the calculator

--- js/calculator/calculator.js
+++ js/calculator/calculator.js
@@ -1,6 +1,24 @@
 const template = document.createElement('template')
 template.innerHTML = `
-<h1>BRUH</>
+<style>
+    #wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    #container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        width: 300px;
+        font-size: 100px;
+    }
+</style>
+
+<div id="wrapper">
+    <div id="container"></div>
+</div>
 `
 customElements.define("my-calculator", 
     class Calculator extends HTMLElement {
@@ -8,9 +26,16 @@ customElements.define("my-calculator",
             super()
             this.attachShadow({ mode: "open"})
             this.shadowRoot.appendChild(template.content.cloneNode(true))
+            this.container = this.shadowRoot.querySelector("#container")
         }
         connectedCallback () {
-
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 5; j++) {
+                    const button = document.createElement("div")
+                    button.textContent = 1
+                    this.container.appendChild(button)
+                }
+            }
         }
     }
 )
