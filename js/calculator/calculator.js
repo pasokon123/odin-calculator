958ea82b03cbdd821416a4620e0a71c2b5f859c5
ae225aw <ae225aw@student.lnu.se>
2025-03-06 20:13:22 +0900

    Add calculate method

--- js/calculator/calculator.js
+++ js/calculator/calculator.js
@@ -11,17 +11,17 @@ template.innerHTML = `
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        width: 300px;
-        font-size: 100px;
+        width: 240px;
+        font-size: 80px;
     }
     #actions {
         display: flex;
         flex-direction: column;
     }
-    button {
-        font-size: 80px;
-        height: 100px;
-        width: 100px;
+    button, #display {
+        font-size: 60px;
+        height: 80px;
+        width: 80px;
     }
 </style>
 
@@ -33,7 +33,8 @@ template.innerHTML = `
         <button id="divide">/</button>
         <button id="multiply">*</button>
     </div>
-    <div id="display">display</div>
+    <button id="equals">=</button>
+    <div id="display"></div>
 </div>
 `
 customElements.define("my-calculator", 
@@ -43,10 +44,15 @@ customElements.define("my-calculator",
             this.attachShadow({ mode: "open"})
             this.shadowRoot.appendChild(template.content.cloneNode(true))
             this.numberButtons = this.shadowRoot.querySelector("#numberButtons")
+            this.additionButton = this.shadowRoot.querySelector("#add")
+            this.subtractionButton = this.shadowRoot.querySelector("#subtract")
+            this.divideButton = this.shadowRoot.querySelector("#divide")
+            this.multiplyButton = this.shadowRoot.querySelector("#multiply") 
+            this.equalsButton = this.shadowRoot.querySelector("#equals")
             this.display = this.shadowRoot.querySelector("#display")
-
-            this.currentValue = 0
-            this.newValue = 0
+          
+            this.firstNumber = 0
+            this.secondNumber = 0
         }
         connectedCallback () {
             for (let i = 9; i > -1; i--) {
@@ -54,8 +60,22 @@ customElements.define("my-calculator",
                 button.setAttribute("id", `number${i}`)
                 button.textContent = i
                 this.numberButtons.appendChild(button)
+                button.addEventListener("click", event => {
+                    if (!this.action && !this.firstNumber) {
+                        this.firstNumber = button.textContent
+                        this.display.textContent = this.firstNumber
+                    } else if (!this.action) {
+                        this.firstNumber += button.textContent
+                        this.display.textContent = this.firstNumber
+                    } else if (!this.secondNumber){
+                        this.secondNumber = button.textContent
+                        this.display.textContent = this.secondNumber
+                    } else {
+                        this.secondNumber += button.textContent
+                        this.display.textContent = this.secondNumber
+                    }
+                })
             }
-         
             this.numberZero = this.shadowRoot.querySelector("#number0")
             this.numberOne = this.shadowRoot.querySelector("#number1")
             this.numberTwo = this.shadowRoot.querySelector("#number2")
@@ -66,6 +86,50 @@ customElements.define("my-calculator",
             this.numberSeven = this.shadowRoot.querySelector("#number7")
             this.numberEight = this.shadowRoot.querySelector("#number8")
             this.numberNine = this.shadowRoot.querySelector("#number9")
+
+            this.additionButton.addEventListener("click", event => {
+                if (this.firstNumber) {
+                    this.action = "+"
+                    this.display.textContent = "+"
+                }
+            })
+            this.subtractionButton.addEventListener("click", event => {
+                if (this.firstNumber) {
+                    this.action = "-"
+                    this.display.textContent = "-"
+                }
+            })
+            this.divideButton.addEventListener("click", event => {
+                if (this.firstNumber) {
+                    this.action = "/"
+                    this.display.textContent = "/"
+                }
+            })
+            this.multiplyButton.addEventListener("click", event => {
+                if (this.firstNumber) {
+                    this.action = "*"
+                    this.display.textContent = "*"
+                }
+            })
+            this.equalsButton.addEventListener("click", event => {
+                if (this.firstNumber && this.secondNumber) {
+                    this.firstNumber = Number(this.firstNumber)
+                    this.secondNumber = Number(this.secondNumber)
+                    this.display.textContent = this.calculate()
+                    this.currentNumber = this.display.textContent
+                }
+            })
+        }
+        calculate () {
+            if (this.action === "+") {
+                return this.add(this.firstNumber, this.secondNumber)
+            } else if (this.action === "-") {
+                return this.subtract(this.firstNumber, this.secondNumber)
+            } else if (this.action === "/") {
+                return this.divide(this.firstNumber, this.secondNumber)
+            } else if (this.action === "*") {
+                return this.multiply(this.firstNumber, this.secondNumber)
+            }
         }
         add (first, second) {
             return first + second
@@ -79,6 +143,6 @@ customElements.define("my-calculator",
         divide (first, second) {
             return first / second
         }
-        
+  
     }
 )
